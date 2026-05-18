0c45a43d6f165bceaf00a062d0949d342a781d07
Savoldelli Andrea <139229454+andrea09000@users.noreply.github.com>
2026-05-18 18:58:19 +0200

    Add files via upload

---
+++ shop-config.js
@@ -0,0 +1,11 @@
+window.SPOTTLY_FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyDibLwVlbGIBs5LYymKNrS-Z7m0t7Ht7vU',
+    authDomain: 'sixsevennnn-dk.firebaseapp.com',
+    projectId: 'sixsevennnn-dk',
+    storageBucket: 'sixsevennnn-dk.firebasestorage.app',
+    messagingSenderId: '159481500389',
+    appId: '1:159481500389:web:9aa45e611d2178c7e43a0c',
+    measurementId: 'G-26X78QB84G'
+};
+
+window.SPOTTLY_FIREBASE_COLLECTION = 'shop_orders';
